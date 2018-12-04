d69b14326d3a84268a1a207bc162dc85f315de29
Werner <egerwern@yahoo.de>
2018-12-04 12:15:34 +0100

    added firmware example

---
+++ firmware/BEL5_DMES_USB_HID_Example/usb_hid/usb/inc/usb_hid.h
@@ -0,0 +1,15 @@
+/*
+ * hid_main.h
+ *
+ *  Created on: 08.11.2017
+ *      Author: Markus Lechner
+ */
+
+#ifndef USB_HID_USB_INC_USB_HID_H_
+#define USB_HID_USB_INC_USB_HID_H_
+/*********************************************************************FUNCTION PROTOTYPE*/
+int usb_config_hid_device(void);
+/**********************************************************************/
+#endif /* USB_HID_USB_INC_USB_HID_H_ */
+
+/* EOF */
